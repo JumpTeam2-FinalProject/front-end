716a09a7af8f254521d1f55a2381633c4850704b
David <djknit@gmail.com>
2021-07-29 23:48:43 -0500

    setting up account info mgmt in top level components

--- src/containers/RestaurantReviews/RestaurantReviews.js
+++ src/containers/RestaurantReviews/RestaurantReviews.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Login from "../../components/Login/Login";
 import ReviewsPage from "../../components/ReviewsPage/ReviewsPage";
@@ -11,20 +11,30 @@ import Restaurants from "../../components/Restaurants/Restaurants";
 import RestaurantPage from "../../components/RestaurantPage/RestaurantPage";
 
 const RestaurantReviews = () => {
+
+    const [currentUser, setCurrentUser] = useState();
+
+    // the following 2 properties are passed as props to every component rendered by this page (both routes and layout)
+    const accountRelatedProps = { currentUser, setCurrentUser };
+
+    const routeRenderFactory = ComponentToRender => (
+        (_props) => <ComponentToRender {..._props} {...accountRelatedProps} />
+    );
+
     let routes = (
         <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/reviews" component={ReviewsPage} />
-            <Route path="/login" component={Login} />
-            <Route path="/account" component={Account} />
-            <Route path="/writereview" component={ReviewPage} />
-            <Route path="/signup" component={SignUp} />
-            <Route path="/restaurants" component={Restaurants} />
-            <Route path="/restaurant/:id" component={RestaurantPage} />
+            <Route path="/" exact render={routeRenderFactory(Home)} />
+            <Route path="/reviews" render={routeRenderFactory(ReviewsPage)} />
+            <Route path="/login" render={routeRenderFactory(Login)} />
+            <Route path="/account" render={routeRenderFactory(Account)} />
+            <Route path="/writereview" render={routeRenderFactory(ReviewPage)} />
+            <Route path="/signup" render={routeRenderFactory(SignUp)} />
+            <Route path="/restaurants" render={routeRenderFactory(Restaurants)} />
+            <Route path="/restaurant/:id" render={routeRenderFactory(RestaurantPage)} />
             <Redirect to="/" />
         </Switch>
     );
-    return <Layout>{routes}</Layout>;
+    return <Layout {...accountRelatedProps}>{routes}</Layout>;
 };
 
 export default RestaurantReviews;
