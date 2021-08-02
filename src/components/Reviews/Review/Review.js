bb76e93bdb43d62d5bdd46294f46cb6803c3f782
Jim Nguyen <vjimnguyen@gmail.com>
2021-08-02 10:36:54 -0700

    Displayed reviews properly

--- src/components/Reviews/Review/Review.js
+++ src/components/Reviews/Review/Review.js
@@ -3,23 +3,29 @@ import { Card, Container, Row, Button } from "react-bootstrap";
 import { TiStarFullOutline, TiStarHalfOutline } from "react-icons/ti";
 
 const Review = (props) => {
-
     var first = null;
     var last = null;
-    var review = null;
+    var date = null;
     var rating = null;
+    var review = null;
 
     if (props.clicked != null) {
-        first = props.firstName.charAt(0).toUpperCase() + props.firstName.slice(1);
+        first =
+            props.firstName.charAt(0).toUpperCase() + props.firstName.slice(1);
         last = props.lastName.charAt(0).toUpperCase() + props.lastName.slice(1);
-        review = props.date;
+        date = props.date;
         rating = props.rating;
-    }
-    else {
-        first = props.user.firstName.charAt(0).toUpperCase() + props.user.firstName.slice(1);
-        last = props.user.lastName.charAt(0).toUpperCase() + props.user.lastName.slice(1);
-        review = props.review.date;
+        review = props.review;
+    } else {
+        first =
+            props.user.firstName.charAt(0).toUpperCase() +
+            props.user.firstName.slice(1);
+        last =
+            props.user.lastName.charAt(0).toUpperCase() +
+            props.user.lastName.slice(1);
+        date = props.review.date;
         rating = props.review.rating;
+        review = props.review.review;
     }
 
     return (
@@ -30,7 +36,7 @@ const Review = (props) => {
                         <span>
                             <p>
                                 {first} {last}&nbsp; - &nbsp;
-                                {review}
+                                {date}
                             </p>
                         </span>{" "}
                         <div
@@ -58,7 +64,7 @@ const Review = (props) => {
                                 alignItems: "center",
                             }}
                         >
-                            <em>"{props.review.review}"</em> &nbsp; &nbsp;
+                            <em>"{review}"</em> &nbsp; &nbsp;
                         </span>
                     </Card.Header>
                     <Card.Body className="mb-0">
